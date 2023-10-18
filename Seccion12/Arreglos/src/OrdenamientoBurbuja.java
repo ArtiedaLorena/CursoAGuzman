1a10077e595e9e5d0e81ea57caa3996cf3654db9
Lorena <artiedalorena@gmail.com>
2023-10-18 14:57:59 -0300

    Ordenamientos

--- Seccion12/Arreglos/src/OrdenamientoBurbuja.java
+++ Seccion12/Arreglos/src/OrdenamientoBurbuja.java
@@ -13,6 +13,22 @@ public class OrdenamientoBurbuja {
             total2--;
         }
     }
+    public static void sortBurbuja(Object[]arreglo){
+        int total = arreglo.length;
+        int contador = 0;
+        for (int i=0; i<total; i++){
+            for(int j=0;j<total-1-i; j++){
+                //Cast a Comparable
+                if( ((Comparable) arreglo[j+1]).compareTo(arreglo[j])<0){
+                    Object auxiliar= arreglo[i];
+                    arreglo[i]=arreglo[j+1];
+                    arreglo[j]=auxiliar;
+                }
+                contador ++;
+            }
+        }
+        System.out.println("contador = " + contador);
+    }
     public static void main(String[] args) {
         //Instanciacion
         //String[] productos = new String[7];
@@ -21,17 +37,21 @@ public class OrdenamientoBurbuja {
                 "Usb 64gb","Notebook Acer","Camara Iqual"
         };
         int total = productos.length;
+        sortBurbuja(productos);
 
-        //Metodo Burbuja
-        for (int i=0; i<total; i++){
-            for(int j=0;j<total; j++){
+
+        //Metodo Burbuja Optimizado
+        /*for (int i=0; i<total-1; i++){
+            for(int j=0;j<total-1-i; j++){
                 if(productos[i].compareTo(productos[j])<0){
                     String auxiliar= productos[i];
-                    productos[i]=productos[j];
+                    productos[i]=productos[j+1];
                     productos[j]=auxiliar;
                 }
+                contador ++;
             }
-        }
+        }*/
+        //System.out.println("contador = " + contador);
 
         //Clase helper Arrays
         //Arrays.sort(productos);
@@ -42,6 +62,17 @@ public class OrdenamientoBurbuja {
 
         }
        //Usando collections// Collections.reverse(Arrays.asList(productos));
-        
-}
+        int[] numeros = new int[4];
+
+        numeros[0]= 1;
+        numeros[1]= 2;
+        numeros[2]= 3;
+        numeros[3]= 4;
+
+
+    for(int i = 0; i< numeros.length;i++){
+        System.out.println("i= " + i + " : "+ numeros[i]);
+    }
+    }
+
 }
